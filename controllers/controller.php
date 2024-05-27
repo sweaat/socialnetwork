4478d3a51f56019e5608a2d3dddff60e77401356
Zedi13 <chatainflorian1@gmail.com>
2024-05-27 13:25:11 +0200

    Remove the hardcoded comments

--- controllers/controller.php
+++ controllers/controller.php
@@ -34,14 +34,10 @@ switch ($action) {
 
     // ===================HARDCODED PART===========================
     // format idPost => array of comments
-    $comments[1] = array(
-      array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 01."),
-      array("nickname" => "FakeUser2", "created_at" => "1970-01-02 00:00:00", "content" => "Fake comment 02."),
-      array("nickname" => "FakeUser1", "created_at" => "1970-01-03 00:00:00", "content" => "Fake comment 03.")
-    );
-    $comments[3] = array(
-      array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 04."),
-    );
+    foreach ($posts as $post) {
+      $postId = $post['id'];
+      $comments[$postId] = GetAllCommentsFromPostId($postId);
+  }
     // =============================================================
 
     include "../views/DisplayPosts.php";
