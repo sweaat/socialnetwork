25a548ffd921b477911deded3e1f35966602e5e5
Zedi13 <chatainflorian1@gmail.com>
2024-05-27 14:27:40 +0200

    Simple registration process

--- models/UserManager.php
+++ models/UserManager.php
@@ -38,3 +38,27 @@ function GetUserIdFromUserAndPassword($username, $password) {
   }
 }
 
+function IsNicknameFree($nickname)
+{
+  global $PDO;
+  $response = $PDO->prepare("SELECT * FROM user WHERE nickname = :nickname ");
+  $response->execute(
+    array(
+      "nickname" => $nickname
+    )
+  );
+  return $response->rowCount() == 0;
+}
+
+function CreateNewUser($nickname, $password)
+{
+  global $PDO;
+  $response = $PDO->prepare("INSERT INTO user (nickname, password) values (:nickname , :password )");
+  $response->execute(
+    array(
+      "nickname" => $nickname,
+      "password" => $password
+    )
+  );
+  return $PDO->lastInsertId();
+}
